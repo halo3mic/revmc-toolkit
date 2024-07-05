110f49b8f79bef54a0ac2105fa1559884637baba
halo3mic <46010359+halo3mic@users.noreply.github.com>
2024-07-05 20:30:36 +0000

    parallel compile

--- crates/builder-revmc/src/build.rs
+++ crates/builder-revmc/src/build.rs
@@ -1,30 +1,35 @@
 use revmc::{EvmLlvmBackend, EvmCompiler, OptimizationLevel};
+use revmc::llvm::inkwell::context::Context;
 use reth_primitives::Bytecode;
 use revm::primitives::SpecId;
+use libloading::Library;
 
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use eyre::{ensure, Ok, Result};
 
 
-const DATA_DIR: &str = ".data";
+const DEFAULT_DATA_DIR: &str = ".data";
 
+/**
+ * Performance considerations:
+ * - Disabled gas metering can improve performance, but it could result in an infinite loop.
+ * - Without length checks performance may be improved, but it could result in undefined behaviour if stack overflows.
+ * - Frame pointers are useful for debugging, but they can be disabled to slightly improve performance.
+ * - Useful for debugging, but it can be disabled for moderate performance improvement.
+ */
 pub struct CompilerOptions {
     out_dir: Option<PathBuf>,
     spec_id: SpecId,
 
-    target: String,
-    target_cpu: Option<String>,
     target_features: Option<String>,
+    target_cpu: Option<String>,
+    target: String,
 
     opt_level: OptimizationLevel,
     no_link: bool,
-    // Disabled gas metering can improve performance, but it could result in an infinite loop.
     no_gas: bool,    
-    // Without length checks performance may be improved, but it could result in undefined behaviour if stack overflows.
     no_len_checks: bool,
-    // Frame pointers are useful for debugging, but they can be disabled to slightly improve performance.
     frame_pointers: bool,
-    // Useful for debugging, but it can be disabled for moderate performance improvement.
     debug_assertions: bool,
 }
 
@@ -53,13 +58,33 @@ impl Default for CompilerOptions {
 }
 
 pub fn compile(label: &str, bytecode: &Bytecode, opt: Option<CompilerOptions>) -> Result<()> {
-    revmc_build::emit();
-
     let opt = opt.unwrap_or_default();
-    let context = revmc::llvm::inkwell::context::Context::create();
-    let target = revmc::Target::new(&opt.target, opt.target_cpu, opt.target_features);
-    let backend = EvmLlvmBackend::new_for_target(&context, true, opt.opt_level, &target)?;
-    // let backend = EvmLlvmBackend::new(&context, true, opt.opt_level)?; - alternative
+    let ctx = Context::create();
+    let mut compiler = create_compiler(label, &ctx, &opt)?;
+
+    let CompilerOptions { out_dir, no_link, spec_id, .. } = opt; 
+    let out_dir = out_dir.map_or_else(
+        || create_default_dir(label),
+        |dir| Ok(dir)
+    )?;
+    
+    compiler.translate(Some(&label), bytecode.bytes_slice(), spec_id)?;
+    let obj = write_precompiled_obj(&mut compiler, label, &out_dir)?;
+
+    if !no_link {
+        link(&obj, &out_dir)?;
+    }
+
+    Ok(())
+}
+
+fn create_compiler<'a>(
+    label: &str, 
+    ctx: &'a Context, 
+    opt: &CompilerOptions
+) -> Result<EvmCompiler<EvmLlvmBackend<'a>>> {
+    let target = revmc::Target::new(&opt.target, opt.target_cpu.clone(), opt.target_features.clone());
+    let backend = EvmLlvmBackend::new_for_target(ctx, true, opt.opt_level, &target)?;
     let mut compiler = EvmCompiler::new(backend);
 
     compiler.set_dump_to(opt.out_dir.clone());
@@ -67,76 +92,54 @@ pub fn compile(label: &str, bytecode: &Bytecode, opt: Option<CompilerOptions>) -
     unsafe { compiler.stack_bound_checks(!opt.no_len_checks) };
     compiler.frame_pointers(opt.frame_pointers);
     compiler.debug_assertions(opt.debug_assertions);
-
+    compiler.inspect_stack_length(true);
     compiler.set_module_name(label);
 
-    
-    compiler.inspect_stack_length(true);
-    // if !stack_input.is_empty() {
-    //     compiler.inspect_stack_length(true);
-    // }
-    let bytecode = bytecode.bytes_slice();
-    compiler.translate(Some(&label), bytecode, opt.spec_id)?;
-    
-    let out_dir = 
-        if let Some(out_dir) = opt.out_dir {
-            out_dir
-        } else {
-            let dir = std::env::current_dir()
-                .expect("Failed to get current directory")
-                .join(DATA_DIR)
-                .join(label);
-            std::fs::create_dir_all(&dir)?;
-            dir
-        };
+    Ok(compiler)
+}
 
-    // Compile.
+fn write_precompiled_obj(
+    compiler: &mut EvmCompiler<EvmLlvmBackend>,
+    label: &str,
+    out_dir: &PathBuf,
+) -> Result<PathBuf> {
     let obj = out_dir.join(label).with_extension("o");
     println!("Writing object file to {}", obj.display());
     compiler.write_object_to_file(&obj)?;
-
     if !obj.exists() {
         return Err(eyre::eyre!("Failed to compile object file"));
     }
-
-    // Link.
-    if !opt.no_link {
-        let so = out_dir.join("a.so");
-        let linker = revmc::Linker::new();
-        linker.link(&so, [obj.to_str().unwrap()])?;
-        ensure!(so.exists(), "Failed to link object file");
-        eprintln!("Linked shared object file to {}", so.display());
-    }
-
-    Ok(())
-
+    Ok(obj)
 }
 
-// pub fn compile2(label: &str, bytecode: &Bytecode) -> Result<()>  {
-//     revmc_build::emit();
-
-//     let out_dir = PathBuf::from(std::env::current_dir()?.join(DATA_DIR).join(label));
-//     if !out_dir.exists() {
-//         std::fs::create_dir_all(&out_dir)?;
-//     } 
-//     let context = revmc::llvm::inkwell::context::Context::create();
-//     let backend = EvmLlvmBackend::new(&context, true, OptimizationLevel::Aggressive)?;
-//     let mut compiler = EvmCompiler::new(backend);
-//     compiler.translate(Some(label), bytecode.bytecode(), SpecId::CANCUN)?;
-//     let object = out_dir.join(label).with_extension("o");
-//     compiler.write_object_to_file(&object)?;
-
-//     cc::Build::new().object(&object).static_flag(true).compile(label);
-
-//     Ok(())
-// }
+fn link(obj: &PathBuf, out_dir: &PathBuf) -> Result<()> {
+    let so = out_dir.join("a.so");
+    let linker = revmc::Linker::new();
+    linker.link(&so, [obj.to_str().unwrap()])?;
+    ensure!(so.exists(), "Failed to link object file");
+    eprintln!("Linked shared object file to {}", so.display());
+    Ok(())
+}
 
 // todo: define path
-pub fn load(label: &str) -> Result<(revmc::EvmCompilerFn, libloading::Library)> {
-    let path = std::env::current_dir()?.join(DATA_DIR).join(label).join("a.so");
+pub fn load(label: &str) -> Result<(revmc::EvmCompilerFn, Library)> {
+    let path = default_dir(label).join("a.so");
     println!("Loading {label} at path {}", path.display());
-    let lib = unsafe { libloading::Library::new(path) }?;
+    let lib = unsafe { Library::new(path) }?;
     let f: libloading::Symbol<'_, revmc::EvmCompilerFn> =
         unsafe { lib.get(label.as_bytes())? };
     Ok((*f, lib))
+}
+
+fn default_dir(label: &str) -> PathBuf {
+    std::env::current_dir()
+        .expect("Failed to get current directory")
+        .join(DEFAULT_DATA_DIR)
+        .join(label)
+}
+
+fn create_default_dir(label: &str) -> Result<PathBuf> {
+    let dir = default_dir(label);
+    std::fs::create_dir_all(&dir)?;
+    Ok(dir)
 }
--- crates/builder-revmc/src/main.rs
+++ crates/builder-revmc/src/main.rs
@@ -2,6 +2,7 @@ pub mod utils;
 mod build;
 mod sim;
 
+use build::CompilerOptions;
 use reth_db::open_db_read_only;
 use reth_provider::{
     providers::StaticFileProvider,
@@ -12,8 +13,7 @@ use reth_revm::database::StateProviderDatabase;
 use revm::{Evm, Database, handler::register::EvmHandler};
 use reth_primitives::{Address, Bytecode as RethBytecode, B256};
 use reth_chainspec::ChainSpecBuilder;
-use revm::primitives::{address, TransactTo, Bytes, U256, Env, Bytecode, spec_to_generic, SpecId};
-use revmc::EvmContext;
+use revm::primitives::{address, Bytes, FixedBytes, TransactTo};
 
 use std::collections::HashMap;
 use std::sync::Arc;
@@ -21,113 +21,152 @@ use std::{path::Path, str::FromStr};
 use eyre::{OptionExt, Result};
 
 
-fn main() -> Result<()> {
+fn main() -> Result<()>{
     dotenv::dotenv().ok();
     
     let db_path = std::env::var("RETH_DB_PATH")?;
     let state_provider = make_state_provider(&db_path)?;
+
+    let contracts = vec![
+        CompileArgs2 {
+            address: address!("f164fC0Ec4E93095b804a4795bBe1e041497b92a"),
+            label: String::from("univ2_router"),
+            options: None,
+        },
+        CompileArgs2 {
+            address: address!("1111111254eeb25477b68fb85ed929f73a960582"),
+            label: String::from("1inch_v5"),
+            options: None,
+        },
+        CompileArgs2 {
+            address: address!("87870bca3f3fd6335c3f4ce8392d69350b4fa4e2"),
+            label: String::from("aave_v3"),
+            options: None,
+        },
+    ];
+    compile_contracts2(state_provider, contracts)?;
     
-    let univ2_router = address!("f164fC0Ec4E93095b804a4795bBe1e041497b92a");
+    
+    std::process::exit(0);
+    
+    // Run
+    
     let label = "univ2_router";
     let code = state_provider.account_code(univ2_router)?
         .ok_or_eyre("No code found for address")?;
-
-    // Compile
-    println!("compiling 👀");
-    build::compile(label, &code, None)?;
-    println!("compiled 🔥");
-
-    // Run
-    let mut env = Env::default();
-    env.tx.transact_to = TransactTo::Call(univ2_router);
-    env.tx.data = Bytes::from_str("0xd06ca61f0000000000000000000000000000000000000000000000000de0b6b3a764000000000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000002000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc20000000000000000000000006982508145454ce325ddbe47a25d4ec3d2311933")?;
-    env.tx.gas_limit = 100_000;
-
-    // run_a(label, env, code)?;
-    // run_b(label, env, state_provider)?;
-    run_c(label, env, code, state_provider)?;
-
-    Ok(())
-}
-
-
-fn run_a(label: &str, env: Env, code: RethBytecode) -> Result<()> {
-    let bytecode = revm::interpreter::analysis::to_analysed(Bytecode::new_raw(
-        Bytes::copy_from_slice(code.bytes_slice()),
-    ));
-    let contract = revm_interpreter::Contract::new_env(&env, bytecode, None);
-    let mut host = revm_interpreter::DummyHost::new(env);
-
-
-    let (f, _lib) = build::load(&label)?;
-    println!("loaded 🚀");
-    let gas_limit = 100_000;
-    let stack_input: Vec<U256> = vec![];
-
-    let mut run = |f: revmc::EvmCompilerFn| {
-        let mut interpreter = revm_interpreter::Interpreter::new(contract.clone(), gas_limit, false);
-        host.clear();
-
-        let (mut ecx, stack, stack_len) = EvmContext::from_interpreter_with_stack(&mut interpreter, &mut host);
-
-        for (i, input) in stack_input.iter().enumerate() {
-            stack.as_mut_slice()[i] = input.into();
-        }
-        *stack_len = stack_input.len();
-
-        let r = unsafe { f.call_noinline(Some(stack), Some(stack_len), &mut ecx) };
-        (r, interpreter.next_action)
-    };
-
-
-    let res = run(f);
-    println!("Result: {:?}", res);
-
-    Ok(())
-}
-
-fn run_b(label: &str, env: Env, state_provider: impl StateProvider + 'static) -> Result<()> {
-    let spdb = StateProviderDatabase::new(state_provider);
-    let mut evm = utils::build_evm(spdb);
-    evm.context.evm.env = Box::new(env);
-
-    let result = evm.transact()?;
-    eprintln!("{:#?}", result.result);
-
-    Ok(())
-}
-
-fn run_c(label: &str, env: Env, code: RethBytecode, state_provider: impl StateProvider + 'static) -> Result<()> {
-    let (external_fun, _lib)  = build::load(&label)?;
-    let external_ctx = ExternalContext::default()
-        .add(code.hash_slow(), external_fun);
     let spdb = StateProviderDatabase::new(state_provider);
 
-    let mut evm = revm::Evm::builder()
-        .with_db(spdb)
+    let mut evm = create_evm(vec![SourceCode {
+        label: label.to_string(),
+        codehash: code.hash_slow(),
+    }], spdb)?;
+
+    evm.context.evm.env.tx.transact_to = TransactTo::Call(univ2_router);
+    evm.context.evm.env.tx.data = Bytes::from_str("0xd06ca61f0000000000000000000000000000000000000000000000000de0b6b3a764000000000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000002000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc20000000000000000000000006982508145454ce325ddbe47a25d4ec3d2311933")?;
+    evm.context.evm.env.tx.gas_limit = 100_000;
+
+    match evm.transact() {
+        Ok(res) =>  eprintln!("{:#?}", res.result),
+        Err(e) => println!("error: {:?}", e),
+    }
+
+    Ok(())
+}
+
+/// Compilation utils
+
+use std::iter::IntoIterator;
+
+struct CompileArgs2 {
+    address: Address,
+    label: String,
+    options: Option<CompilerOptions>,
+}
+
+impl CompileArgs2 {
+
+    fn into_compile_args(self, state_provider: &impl StateProvider) -> Result<CompileArgs> {
+        let code = state_provider.account_code(self.address)?
+            .ok_or_eyre("No code found for address")?;
+        Ok(CompileArgs {
+            label: self.label,
+            code,
+            options: self.options,
+        })
+    }
+
+}
+
+fn compile_contracts2(
+    state_provider: Arc<impl StateProvider>,
+    contracts: impl IntoIterator<Item=CompileArgs2>
+) -> Result<Vec<Result<()>>> {
+    let contracts = contracts.into_iter()
+        .map(|c| c.into_compile_args(&state_provider))
+        .collect::<Result<Vec<_>>>()?;
+    let results = compile_contracts(contracts);
+    Ok(results)
+}
+
+/// Compilation & Utils - Library
+
+use rayon::prelude::*;
+
+struct CompileArgs {
+    label: String,
+    code: RethBytecode,
+    options: Option<CompilerOptions>,
+}
+
+fn compile_contracts(args: Vec<CompileArgs>) -> Vec<Result<()>> {
+    args.into_par_iter()
+        .map(|arg| compile_contract(arg))
+        .collect()
+}
+
+fn compile_contract(arg: CompileArgs) -> Result<()> {
+    build::compile(&arg.label, &arg.code, arg.options)
+}
+
+// Simulation utils
+
+use libloading::Library;
+
+// todo: can label just be the codehash?
+struct SourceCode {
+    label: String,
+    codehash: FixedBytes<32>,
+}
+
+fn create_evm<E>(
+    src_codes: Vec<SourceCode>, 
+    db: impl Database<Error=E> + 'static
+) -> Result<Evm<'static, ExternalContext, impl Database<Error=E>>> 
+where E: std::fmt::Debug + 'static
+{
+    let mut external_ctx = ExternalContext::default();
+    for src_code in src_codes {
+        let fnc  = build::load(&src_code.label)?;
+        external_ctx.add(src_code.codehash, fnc);
+    }
+    let evm = revm::Evm::builder()
+        .with_db(db)
         .with_external_context(external_ctx)
         .append_handler_register(register_handler)
         .build();
-    evm.context.evm.env = Box::new(env);
-
-    let result = evm.transact()?;
-    eprintln!("{:#?}", result.result);
-
-    Ok(())
+    Ok(evm)
 }
 
 #[derive(Default)]
-pub struct ExternalContext(HashMap<B256, revmc::EvmCompilerFn>);
+pub struct ExternalContext(HashMap<B256, (revmc::EvmCompilerFn, Library)>);  // todo: consider fast hashmap
 
 impl ExternalContext {
-    fn add(self, code_hash: B256, fnc: revmc::EvmCompilerFn) -> Self {
-        let mut map = self.0;
-        map.insert(code_hash, fnc);
-        Self(map)
+    fn add(&mut self, code_hash: B256, fnc: (revmc::EvmCompilerFn, Library)) {
+        self.0.insert(code_hash, fnc);
     }
 
     fn get_function(&self, bytecode_hash: B256) -> Option<revmc::EvmCompilerFn> {
-        self.0.get(&bytecode_hash).map(|f| *f)
+        self.0.get(&bytecode_hash).map(|f| f.0)
     }
 }
 
@@ -145,7 +184,9 @@ fn register_handler<DB: Database + 'static>(handler: &mut EvmHandler<'_, Externa
     });
 }
 
-fn make_state_provider(db_path: &str) -> Result<impl StateProvider> {
+// Benchmarking utils
+
+fn make_state_provider(db_path: &str) -> Result<Arc<impl StateProvider>> {
     let db_path = Path::new(db_path);
     let db = open_db_read_only(db_path.join("db").as_path(), Default::default())?;
 
@@ -154,18 +195,7 @@ fn make_state_provider(db_path: &str) -> Result<impl StateProvider> {
     let factory = ProviderFactory::new(db, spec.into(), stat_file_provider);
     let state_provider = factory.latest()?;
 
-    Ok(state_provider)
+    Ok(Arc::new(state_provider))
 }
 
-fn compile_contract(label: &str, provider: impl StateProvider, address: Address) -> Result<()> {
-    let code = provider.account_code(address)?.ok_or_eyre("No code found for address")?;
-    build::compile(label, &code, None)
-}
-
-// todo: impl as benches
-fn native_sim() {}
-
-fn aot_sim() {}
-
-fn jit_sim() {}
 
