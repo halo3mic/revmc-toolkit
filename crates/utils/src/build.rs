b80dae2e5da6d72662f10973b98148879644d1dc
halo3mic <46010359+halo3mic@users.noreply.github.com>
2024-09-28 09:30:19 +0000

    move build utils

--- crates/utils/src/build.rs
+++ crates/utils/src/build.rs
@@ -3,10 +3,10 @@ use serde::{Deserialize, Deserializer};
 use reth_provider::StateProvider;
 use revm::primitives::Address;
 use std::iter::IntoIterator;
+use serde_json::Value;
 use revm::primitives::B256;
 use revmc::EvmCompilerFn;
 use std::path::PathBuf;
-use serde_json::Value;
 
 use eyre::{OptionExt, Result};
 
@@ -43,4 +43,100 @@ fn fetch_code_for_account(state_provider: &impl StateProvider, account: Address)
         .ok_or_eyre("No code found for address")?;
     let code_bytes = code.original_byte_slice().to_vec();
     Ok(code_bytes)
+}
+
+pub fn compile_aot_from_codes(
+    codes: Vec<Vec<u8>>,
+    fallback_opt: Option<CompilerOptions>,
+) -> Result<Vec<Result<()>>>
+{
+    let contracts = codes.into_iter().map(|c| c.into()).collect();
+    revmc_toolbox_build::compile_contracts_aot(contracts, fallback_opt)
+}
+
+pub fn compile_jit_from_codes(
+    codes: Vec<Vec<u8>>,
+    fallback_opt: Option<CompilerOptions>,
+) -> Result<Vec<Result<(B256, EvmCompilerFn)>>> {
+    let contracts = codes.into_iter().map(|c| c.into()).collect();
+    revmc_toolbox_build::compile_contracts_jit_par(contracts, fallback_opt)
+}
+
+pub fn compile_aot_from_file_path(
+    state_provider: &Box<impl StateProvider + ?Sized>,
+    file_path: &PathBuf,
+) -> Result<Vec<Result<()>>> {
+    let config_txt = std::fs::read_to_string(file_path)?;
+    let build_file = serde_json::from_str(&config_txt)?;
+    compile_aot_from_build_file(state_provider, build_file)
+}
+
+pub fn compile_aot_from_build_file(
+    state_provider: &Box<impl StateProvider + ?Sized>,
+    build_file: BuildFile,
+) -> Result<Vec<Result<()>>> {
+    let (contracts, fconfig) = build_file.into_contracts_and_fconfig(state_provider)?;
+    revmc_toolbox_build::compile_contracts_aot(contracts, fconfig)
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct BuildObject {
+    pub address: Option<Address>,
+    #[serde(default, deserialize_with = "hex_or_vec")]
+    pub code: Option<Vec<u8>>,
+    pub options: Option<CompilerOptions>,
+    pub _label: Option<String>, // todo: label servers no purpose
+}
+
+#[derive(Debug, Clone, serde::Deserialize)]
+pub struct BuildFile {
+    contracts: Vec<BuildObject>, 
+    #[serde(rename = "fallbackConfig")]
+    fallback_config: Option<CompilerOptions>,
+}
+
+impl BuildFile {
+
+    pub fn into_contracts_and_fconfig(
+        self, 
+        state_provider: &Box<impl StateProvider + ?Sized>
+    ) -> Result<(Vec<CodeWithOptions>,  Option<CompilerOptions>)> {
+        // todo: check for duplicates
+        let contracts = self.contracts.into_iter()
+            .map(|c| {
+                if let Some(address) = c.address {
+                    let code = fetch_code_for_account(state_provider, address)?;
+                    Ok(CodeWithOptions { code, options: c.options })
+                } else if let Some(code) = c.code {
+                    Ok(CodeWithOptions { code, options: c.options })
+                } else {
+                    return Err(eyre::eyre!("No address or code found"));
+                }
+            })
+            .collect::<Result<Vec<_>>>()?;
+        Ok((contracts, self.fallback_config))
+    }
+
+}
+
+
+fn hex_or_vec<'de, D>(deserializer: D) -> Result<Option<Vec<u8>>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let value = Option::<Value>::deserialize(deserializer)?;
+    
+    match value {
+        Some(Value::String(s)) => {
+            let s = s.trim_start_matches("0x");
+            Some(hex::decode(s).map_err(serde::de::Error::custom)).transpose()
+        },
+        Some(Value::Array(arr)) => Some(arr
+            .into_iter()
+            .map(|v| v.as_u64().ok_or_else(|| serde::de::Error::custom("Expected a number"))
+                .and_then(|n| u8::try_from(n).map_err(serde::de::Error::custom)))
+            .collect()).transpose(),
+        Some(_) => Err(serde::de::Error::custom("Expected hex string or array of numbers")),
+        None => Ok(None),
+    }
 }
